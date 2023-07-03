08197d839b65d5a3b047cb75cdcd00aedd3a3e15
Maks <max.wp.com@gmail.com>
2023-07-03 13:26:43 +0200

    Initial commit

---
+++ app/oauth2.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+
+SECRET_KEY = "hello"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+
+    return encoded_jwt
+
---
+++ app/routers/user.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..database import get_db
+from .. import schemas, models
+from typing import List
+from .. import utils
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@router.get("/", response_model=List[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return users
+
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
